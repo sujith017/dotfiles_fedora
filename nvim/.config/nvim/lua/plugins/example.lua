bcf39d4b048017b3a564054f9990ac9675ee244d
sujith017 <sujithb934@gmail.com>
2025-08-05 10:08:24 +0530

    config updates

--- nvim/.config/nvim/lua/plugins/example.lua
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -64,53 +64,53 @@ return {
   },
 
   -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
-
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- pyright will be automatically installed with mason and loaded with lspconfig
+  --       pyright = {},
+  --     },
+  --   },
+  -- },
+  --
   -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-        tsserver = {},
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
-
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "jose-elias-alvarez/typescript.nvim",
+  --     init = function()
+  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
+  --         -- stylua: ignore
+  --         vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+  --       end)
+  --     end,
+  --   },
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- tsserver will be automatically installed with mason and loaded with lspconfig
+  --       tsserver = {},
+  --     },
+  --     -- you can do any additional lsp server setup here
+  --     -- return true if you don't want this server to be setup with lspconfig
+  --     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+  --     setup = {
+  --       -- example to setup with typescript.nvim
+  --       tsserver = function(_, opts)
+  --         require("typescript").setup({ server = opts })
+  --         return true
+  --       end,
+  --       -- Specify * to use this function as a fallback for any server
+  --       -- ["*"] = function(server, opts) end,
+  --     },
+  --   },
+  -- },
+  --
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
   { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -194,7 +194,8 @@ return {
       },
     },
   },
-}, {
+},
+{
   "lewis6991/gitsigns.nvim",
   event = "LazyFile",
   opts = {
@@ -250,39 +251,42 @@ return {
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
     end,
   },
-}, {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "InsertEnter",
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
-}, {
-  "zbirenbaum/copilot.lua",
-  opts = function()
-    LazyVim.cmp.actions.ai_accept = function()
-      if require("copilot.suggestion").is_visible() then
-        LazyVim.create_undo()
-        require("copilot.suggestion").accept()
-        return true
-      end
-    end
-  end,
-}, {
+},
+--   {
+--   "zbirenbaum/copilot.lua",
+--   cmd = "Copilot",
+--   build = ":Copilot auth",
+--   event = "InsertEnter",
+--   opts = {
+--     suggestion = {
+--       enabled = not vim.g.ai_cmp,
+--       auto_trigger = true,
+--       keymap = {
+--         accept = false, -- handled by nvim-cmp / blink.cmp
+--         next = "<M-]>",
+--         prev = "<M-[>",
+--       },
+--     },
+--     panel = { enabled = false },
+--     filetypes = {
+--       markdown = true,
+--       help = true,
+--     },
+--   },
+-- },
+--   {
+--   "zbirenbaum/copilot.lua",
+--   opts = function()
+--     LazyVim.cmp.actions.ai_accept = function()
+--       if require("copilot.suggestion").is_visible() then
+--         LazyVim.create_undo()
+--         require("copilot.suggestion").accept()
+--         return true
+--       end
+--     end
+--   end,
+-- },
+{
   "neovim/nvim-lspconfig",
   opts = {
     -- make sure mason installs the server
@@ -455,7 +459,8 @@ return {
       end,
     },
   },
-}, {
+},
+{
   "williamboman/mason.nvim",
   opts = { ensure_installed = { "prettier" } },
 }
